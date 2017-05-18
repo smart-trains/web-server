1ebc22db9488204b116f08a4a64947a1ea686974
ra <rayax86@gmail.com>
2017-05-18 19:02:37 +1000

    writes data to sqlite db

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var api = require('./routes/api');
 
 var app = express();
 
+var initDb = require('./lib/initDb');
+
+initDb();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -29,20 +33,20 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,63 @@
 var express = require('express');
 var router = express.Router();
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(':smart_trains:')
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('../data/:smart_trains:');
 
 /* POST receive ip reported by raspberry pi. */
 router.post('/rpi_ip', function(req, res, next) {
-  	console.log(req.body);
+	var json = req.body;
+
+    db.serialize(function () {
+        var stmt = db.prepare('INSERT INTO raspberry_ip VALUES (?, ?)');
+		stmt.run(json.ssid, json.ip);
+        stmt.finalize();
+    });
+
+    db.close();
 	res.send();
+});
+
+router.post('/temp', function(req, res, next) {
+    var json = req.body;
+
+    db.serialize(function () {
+        var stmt = db.prepare('INSERT INTO temperature VALUES (?)');
+        stmt.run(json.temperature);
+        stmt.finalize();
+    });
+
+    db.close();
+    res.send();
+});
+
+router.get('/rpi_ip', function(req, res, next) {
+	var json = [];
+
+    db.serialize(function () {
+        var stmt = db.prepare('SELECT ssid, ip, datetime(time,"unixepoch") as time from raspberry_ip order by time desc limit 10');
+        stmt.each(function(err, row) {
+			json.push(row);
+        })
+    }, function() {
+        res.json(json);
+    });
+
+    db.close();
+});
+
+router.get('/temp', function(req, res, next) {
+    var json = [];
+
+    db.serialize(function () {
+        var stmt = db.prepare('SELECT temperature, datetime(time,"unixepoch") as time from temperature order by time desc limit 10');
+        stmt.each(function(err, row) {
+            json.push(row);
+        })
+    }, function() {
+        res.json(json);
+    });
+
+    db.close();
 });
 
 module.exports = router;
