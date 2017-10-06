3c72b62431f62b71ff25b4103ef5a96caeff779c
ra <rayax86@gmail.com>
2017-10-07 01:01:11 +1100

    removed set schema to app given database has set to include app

--- imports/startup/server/init.js
+++ imports/startup/server/init.js
@@ -1,7 +1,4 @@
 /**
  * Created by ra on 4/10/2017.
  */
-import mpg from 'meteor-pg';
-
-const setSchema = Meteor.bindEnvironment(() => mpg.none('SET search_path TO app'));
-setInterval(setSchema, 300);
+import 'meteor-pg';
